c4270b368bfa88acf5194217afe11b509a0b1280
chase <53500545+m-kyu@users.noreply.github.com>
2023-03-28 17:54:08 +0900

    asdf

---
+++ pages/about.vue
@@ -0,0 +1,28 @@
+<template lang="">
+    <div>
+        <h2>About</h2>
+
+        <p>{{count}}</p>
+        <button @click="incre">증가</button>
+
+    </div>
+</template>
+
+<script setup>
+    const aa = useState('g',()=>'about내용쪽에 있는 데이터....');
+</script>
+
+<script>
+export default {
+    data(){
+        return{
+            count:100
+        }
+    },
+    methods:{
+        incre(){
+            this.count++;           
+        }
+    }
+}
+</script>
