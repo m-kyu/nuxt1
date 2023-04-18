fec19f6a6882b793b6ceaa9424cdd59cb9a43152
chase <53500545+m-kyu@users.noreply.github.com>
2023-04-18 14:56:44 +0900

    asdf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css:['~/assets/css/_common.scss']
+    css:['~/assets/css/_common.scss'],
+    router: {
+        base: '/nuxt2/'
+    }
 })
